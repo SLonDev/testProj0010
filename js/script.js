1b4cd370a3c93986cba98b37d4420f946c711db8
vladimir.polishchuk <vladimir.polishchuk@ringcentral.com>
2023-11-14 14:09:38 +0100

    test commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+a = 33;
+console.log(a);
+
+
+console.log(66);
+let number = 5; // переменная изменяема
+const leftBorderfWidth = 1;
+
+// number = 10;
+// console.log(number);
+// leftBorderfWidth = 22;
+// console.log(leftBorderfWidth);
